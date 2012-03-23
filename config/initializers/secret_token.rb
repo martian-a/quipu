df3cb57cc228e199574e5851a709e69f110ff22b
martian-a <coder@bluegumtree.com>
2012-03-23 16:50:43 +0000

    Auto-generated rails app framework.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quipu::Application.config.secret_token = '8e5495163cae43252345fc9a8471c421840cd323efe92e02583cff47a10f303ea95df8bb38c7bab4716664877f06eeaa0b44e102b994b842459069a4d8a7db83'
